5b0f6421c7a555daf9f382d391753a2b9b811397
mokoenal <lindiwemokoena374@gmail.com>
2023-09-16 15:57:33 +0200

    added to prep2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,40 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("Enter a grade percentage? ");
+        string answerFromUser= Console.ReadLine();
+        int percent = int.Parse(answerFromUser);
+
+        string letter = "";
+
+        if (percent >= 90)
+        {
+            letter = "A";
+        }
+        else if (percent >= 80)
+        {
+            letter = "B";
+        }
+        else if (percent >= 70)
+        {
+            letter = "C";
+        }
+        else if (percent >= 60) 
+       {
+            letter = "D";
+       }
+        else 
+       {
+            letter = "F";
+       }
+       Console.WriteLine($"Your grade is: {letter}");
+        if (percent >= 70)
+       {
+        Console.WriteLine("Congratulation, you have Passed!");
+       }
+        else
+       {
+        Console.WriteLine("Keep trying, you will make it next time!");
+       }
     }
 }
