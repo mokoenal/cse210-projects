9994a55ddfc5af00af818ff1269e73fe5e1f5905
mokoenal <lindiwemokoena374@gmail.com>
2023-09-21 20:02:05 +0200

    added to the program

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -3,7 +3,47 @@ using System;
 class Program
 {
     static void Main(string[] args)
-    {
-        Console.WriteLine("Hello Prep3 World!");
+
+
+    {   //Generate a random magic number between 1 to 100
+        Random random = new Random();
+        int magicNumber = random.Next(1, 101);
+        
+        int userGuesse;
+
+        //part One & two
+        //Console.Write("What is the magic number? ");
+        //int magicNumber = int.Parse(Console.ReadLine());
+
+        //Ask the user to guesse the number & add a loop that keeps looping until they guesse it right
+
+        while(true)
+        {
+        
+        Console.Write("What is your guesse? ");
+        userGuesse = int.Parse(Console.ReadLine());
+
+        //check if the user needs to guesse higher or lower or if they guessed it corrently
+        if (userGuesse < magicNumber)
+    
+        {
+            Console.WriteLine("Higher");
+        }
+        else if (userGuesse > magicNumber)
+
+        {
+            Console.WriteLine("Lower");
+        }
+
+        else
+        {
+            Console.WriteLine("You guessed it!");
+            break;//Exit the loop when they guesse it correnty
+        }
+        
+        }
+
+
     }
+    
 }
--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -4,6 +4,59 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+       List <double> numbers = new List <double>();
+       Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+
+       while(true)
+       {
+        Console.Write("Enter number: ");
+        double input = Convert.ToDouble(Console.ReadLine());
+
+        if (input == 0)
+        break;
+
+        numbers.Add(input);
+
+       }
+
+        if (numbers.Count > 0)
+
+        {
+            double sum = 0;
+            foreach (double number in numbers)
+            {
+                sum += number;
+        }
+        double average = sum / numbers.Count;
+        double max = FindMax(numbers);
+
+        Console.WriteLine($"The sum is: {sum}");
+        Console.WriteLine($"The average is: {average}");
+        Console.WriteLine($"The largest number is: {max}");
+        }
+
+        else
+        
+        {
+            Console.WriteLine("No numbers entered.");
+        }
+    }
+
+    static double FindMax(List<double> numbers)
+    {
+        double max = double.MinValue;
+        foreach (double number in numbers)
+        {
+            if (number > max)
+                max = number;
+        }
+        return max;
     }
 }
+
+
+
+
+
+
+
